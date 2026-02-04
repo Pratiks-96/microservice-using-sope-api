79d9f89338afea915f6143030643686fc07a46b5
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-04 20:25:33 +0530

    Create package-info.java

---
+++ backend/src/main/java/com/example/soapservice/model/package-info.java
@@ -0,0 +1,5 @@
+@jakarta.xml.bind.annotation.XmlSchema(
+    namespace = "http://example.com/soapservice",
+    elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED
+)
+package com.example.soapservice.model;
