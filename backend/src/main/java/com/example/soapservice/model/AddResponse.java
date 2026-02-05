85b4b1311bbca28f87cf555357e8a54993f44d0f
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-05 18:43:14 +0530

    Update AddResponse.java

--- backend/src/main/java/com/example/soapservice/model/AddResponse.java
+++ backend/src/main/java/com/example/soapservice/model/AddResponse.java
@@ -1,4 +1,29 @@
 package com.example.soapservice.model;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bi
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "AddResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AddResponse {
+
+    @XmlElement(name = "result")
+    private int result;
+
+    public AddResponse() {
+    }
+
+    public AddResponse(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
